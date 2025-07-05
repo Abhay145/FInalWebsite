0e8d432d7ed421dd42aa93840d219adf1306ad13
SuyashShukla0007 <123103079@nitkkr.ac.in>
2025-07-05 16:18:54 +0530

    wrote api to get role

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -1,6 +1,6 @@
 // src/routes/adminRoutes.js
 const express = require('express');
-const { registerAdmin, loginAdmin, getAllStudents, getAllProfessors, assignBasedOnChoices,assignFallbackElectives,clearSubjectsForAllStudents, getstudents} = require('../controllers/adminController.js');
+const { registerAdmin, loginAdmin, getAllStudents, getAllProfessors, assignBasedOnChoices,assignFallbackElectives,clearSubjectsForAllStudents, getstudents,checkAdminRole} = require('../controllers/adminController.js');
 const { authMiddleware, adminMiddleware } = require('../middleware/authMiddleware.js');
 
 const router = express.Router();
@@ -11,7 +11,7 @@ router.get('/students', authMiddleware, adminMiddleware, getAllStudents);
 router.get('/professors', authMiddleware, adminMiddleware, getAllProfessors);
 router.post('/chosen-electives', authMiddleware, adminMiddleware,assignBasedOnChoices );
 router.post('/nonchosen-electives', authMiddleware, adminMiddleware,assignFallbackElectives );
-
+router.get('/check-admin', checkAdminRole);
 
 router.post('/substudents', getstudents );
 router.delete('/electives',clearSubjectsForAllStudents );
